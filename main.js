0b29ec3f2e5429084cbd7331a239a8fa8254ba4c
Johnny Saniat <Jcsaniat@gmail.com>
2023-07-20 14:05:42 -0500

    got cards on the DOM

--- main.js
+++ main.js
@@ -1,50 +1,68 @@
 const items = [
   {
     id: 1,
-    name: AMETHYST Base 65 Keyboard,
+    name: "AMETHYST Base 65 Keyboard",
     availability: "Available",
-    specialSkill: "Gives sincere apologies.",
-    type: "cat",
+    size: "Our signature 65% keyboard",
+    weight: "1000g",
+    whiteFlame: "White Flame Switches: $120.00",
+    titanHeart: "Titan Heart Switches: $135.00",
+    geoLinear: "Geo Linear Switches: $150.00",
     imageUrl: "https://higround.co/cdn/shop/products/HiGround_Crystal_redone_019-min.jpg?v=1679955627",
   },
   {
-      id: 2,
-    name: "Trouble",
-    color: "Brown",
-    specialSkill: "Just picks the tomatoes off of a sandwich instead of requesting a whole new sandwich.",
-    type: "dino",
+    id: 2,
+    name: "AMETHYST Base 65 Keyboard",
+    availability: "Available",
+    size: "Our signature 65% keyboard",
+    weight: "1000g",
+    whiteFlame: "White Flame Switches: $120.00",
+    titanHeart: "Titan Heart Switches: $135.00",
+    geoLinear: "Geo Linear Switches: $150.00",
     imageUrl: "https://higround.co/cdn/shop/products/HiGround_Crystal_redone_037-min.jpg?v=1679957856",
   },
   {
     id: 3,
-    name: "Whiskers",
-    color: "Yellow",
-    specialSkill: "Can prove he is a real man by drinking whiskey.",
-    type: "dino",
+    name: "AMETHYST Base 65 Keyboard",
+    availability: "Available",
+    size: "Our signature 65% keyboard",
+    weight: "1000g",
+    whiteFlame: "White Flame Switches: $120.00",
+    titanHeart: "Titan Heart Switches: $135.00",
+    geoLinear: "Geo Linear Switches: $150.00",
     imageUrl: "https://higround.co/cdn/shop/products/HiGROUND_GUNDAM_batch1_048.jpg?v=1670530360"
   },
   {
     id: 4,
-    name: "Coco",
-    color: "Black",
-    specialSkill: "Burps minimally.",
-    type: "dog",
+    name: "AMETHYST Base 65 Keyboard",
+    availability: "Available",
+    size: "Our signature 65% keyboard",
+    weight: "1000g",
+    whiteFlame: "White Flame Switches: $120.00",
+    titanHeart: "Titan Heart Switches: $135.00",
+    geoLinear: "Geo Linear Switches: $150.00",
     imageUrl: "https://higround.co/cdn/shop/products/HiGROUND_STREETFIGHTER_RESHOT_001.jpg?v=1681507381"
   },
   {
     id: 5,
-    name: "Spooky",
-    color: "Brown",
-    specialSkill: "Comfortable in the outdoors for up to eight hours.",
-    type: "cat",
+    name: "AMETHYST Base 65 Keyboard",
+    availability: "Available",
+    size: "Our signature 65% keyboard",
+    weight: "1000g",
+    whiteFlame: "White Flame Switches: $120.00",
+    titanHeart: "Titan Heart Switches: $135.00",
+    geoLinear: "Geo Linear Switches: $150.00",
     imageUrl: "https://higround.co/cdn/shop/products/HIGROUNDx100THEIVES_028_57d71d8c-fd7d-4299-9bb0-ff944fcc63c2.jpg?v=1668731923"
   },
   {
     id: 6,
-    name: "Tiger",
-    color: "Black",
-    specialSkill: "Can read (but cannot understand) Hebrew.",
-    type: "dog",
+    name: "AMETHYST Base 65 Keyboard",
+    availability: "Available",
+    size: "Our signature 65% keyboard",
+    weight: "1000g",
+    whiteFlame: "White Flame Switches: $120.00",
+    titanHeart: "Titan Heart Switches: $135.00",
+    geoLinear: "Geo Linear Switches: $150.00",
     imageUrl: "https://higround.co/cdn/shop/products/HIGROUNDx100THEIVES_044_f4ebf1cd-f51d-482c-b203-5ba25b086169.jpg?v=1668731740"
   }
 ];
@@ -61,26 +79,40 @@ const cardsOnDom = (array) => {
 
   for (const item of array ) {
     domString += `
+    <div class="container">
     <div class="card" style="width: 18rem;">
     <header id ="title">
     <h2>${item.name}</h2>
     </header>
-    <div class="card-body">
-    <img src="${item.imageUrl}" class="card-img-top" alt="${item.name}>"
-    <p class="description"> ${product.availability}</p>
-    
-    
-    </div>
-    <p class ="specs"
-    <img src="${item.imageUrl}" class="card-img-top" alt=${item.name}>
-    <div class="card-body">
-      <h5 class="card-title">${item.name}</h5>
-      <p class="card-text">${item.color}</p>
-      <p class="card-text">${item.specialSkill}</p>
-      <p class="card-text">${item.type}</p>
-      <p class="card-text">${item.id} </p>
-    </div>
-  </div>`
+
+    <section class="sections">
+      <div class="card-body">
+        <img src="${item.imageUrl}" class="card-img-top" alt="${item.name}>"
+        <p class="description"> ${item.availability}</p>
+      </div>
+    </section>
+
+    <section class="sections">
+      <header>
+      <h5>Specifications</h5>
+      </header>
+      <p class ="specifications"> Size: ${item.size} </p>
+      <p class ="specifications"> Size: ${item.weight} </p>
+      <footer>
+        <p class="valid"> Specifications are valid until until 7/30/2023</p>
+      </footer>
+      </section>
+
+      <section class="sections">
+      <header>
+      <h5>Pricing</h5>
+      </header>
+      <p class="price">${item.whiteFlame}</p>
+      <p class="price">${item.titanHeart}</p>
+      <p class="price">${item.geoLinear}</p>
+      </section>
+   </div>
+   </div>`
   }
 
   renderToDom("#app", domString);
